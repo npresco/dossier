b8f9f9e6fbfa3cffa26517a61128cf172535dd9a
Noah Prescott <noah.prescott@gmail.com>
2022-10-04 22:16:09 -0400

    Use original version of connection adapter

--- lib/dossier/adapter/active_record.rb
+++ lib/dossier/adapter/active_record.rb
@@ -14,32 +14,13 @@ module Dossier
       end
 
       def execute(query, report_name = nil)
-        retries ||= 0
-
-        connection.verify!
+        # Ensure that SQL logs show name of report generating query
         Result.new(connection.exec_query(*["\n#{query}", report_name].compact))
-      rescue PG::ConnectionBad => e
-        if retries < 3
-          retries += 1
-
-          logger.error("Dossier bad connection: #{e.message}. Retrying #{3 - retries} times.")
-
-          # Attempt to acquire a new connection
-          self.connection = active_record_connection
-
-          retry
-        else
-          raise Dossier::ExecuteError.new "#{e.message}\n\n#{query}"
-        end
       rescue => e
         raise Dossier::ExecuteError.new "#{e.message}\n\n#{query}"
       end
 
       private
-
-      def logger
-        defined?(Rails) ? Rails.logger : Logger.new($stdout)
-      end
 
       def active_record_connection
         @abstract_class = Class.new(::ActiveRecord::Base) do
